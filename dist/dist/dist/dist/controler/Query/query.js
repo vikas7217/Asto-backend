4331dd1d43dbedf538ab4d6252f491a7791ad966
vikas kumar <vikas.kumar2@method-hub.com>
2025-07-15 17:58:35 +0530

    test new build fixed deploy setting

---
+++ dist/dist/dist/dist/controler/Query/query.js
@@ -0,0 +1,24 @@
+const {
+  inquiryMail
+} = require('../../auth/mailer');
+const Query = require('../../module/query');
+exports.CreateQuery = async (req, res) => {
+  try {
+    const query = await Query.create(req.body);
+    console.log(query);
+    if (query) {
+      const mail = inquiryMail(req.body);
+      console.log(mail, '====================');
+    }
+    query.save();
+    res.status(200).json({
+      isSuccess: true,
+      message: 'your query is send to the Astrologer we give you the response as soon as possible'
+    });
+  } catch (error) {
+    res.status(400).json({
+      isSuccess: false,
+      error
+    });
+  }
+};
